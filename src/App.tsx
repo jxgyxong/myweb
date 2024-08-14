f9fe5fddaba99318125cd9eb0a264066cb7a33d8
wprud <pjegyeong@gmail.com>
2024-08-14 09:49:43 +0900

    박제경 씨발

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={process.env.PUBLIC_URL + "/img/물.png"}/>
+        <p>
+          물뚱뚱이
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
